e46e72eeb508de9db7701d9e7f9c4b4c9fcd7b4b
Ezra Sandzer-Bell and Getro Naissance <ezra@>
2016-12-06 10:15:10 -0800

    pass rspec for retrieval of all value categories within Patient class

--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -11,4 +11,16 @@ describe(Patient) do
       expect(@test_patient.name).to(eq("Paul"))
     end
   end
+
+  describe('#birthday') do
+    it('will return birthday of the patient') do
+      expect(@test_patient.birthday).to(eq('1987-02-26 00:00:00'))
+    end
+  end
+
+  describe('#doctor_id') do
+    it('will return the doctor id assigned to patient') do
+      expect(@test_patient.doctor_id).to(eq(1))
+    end
+  end
 end
